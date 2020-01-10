9d69ec1f327b2a7d5471a670deaf4376e3ccf358
Nathan <nmattick@gmail.com>
2020-01-10 06:25:24 +0000

    Reformatting

--- index.php
+++ index.php
@@ -1,19 +1,22 @@
 <!DOCKTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Document</title>
-	<style type="text/css">
-		header{
-			background: #e3e3e3e
-		}
-	</style>
-</head>
-<body>
-<header>
-	<h1>
-		<?php echo "Hello, ".$_GET['name']; ?>
-	</h1>
-</header>
-</body>
-</html>
+	<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		<title>Document</title>
+		<style type="text/css">
+			header {
+				background: #e3e3e3e
+			}
+		</style>
+	</head>
+
+	<body>
+		<header>
+			<h1>
+				<?php echo "Hello, " . $_GET['name']; ?>
+			</h1>
+		</header>
+	</body>
+
+	</html>

cce9bec54b40c09e7e8fe8fa5037ada4d768055a
Nathan <nmattick@gmail.com>
2020-01-14 06:25:48 +0000

    Using if/then and ? operations

--- index.view.php
+++ index.view.php
@@ -23,6 +23,32 @@
         <li>
             <strong>Status: </strong> <?=$task['completed'] ? 'Done' : 'Not done';?>
         </li>
+
+        <li>
+            <strong>Status: </strong>
+            <?php
+if ($task['completed']) {
+    echo "Done";
+} else {
+    echo "Not done";
+}
+?>
+        </li>
+
+        <li>
+            <strong>Status: </strong>
+            <?=$task['completed'] ? '&#9989' : '&#10062';?>
+        </li>
+
+        <li>
+            <strong>Status: </strong>
+            <?php if ($task['completed']): ?>
+                <span class="icon">&#9995</span>
+            <?php else: ?>
+                <span class="icon">&#9994</span>
+            <?php endif;?>
+    </li>
+
     </ul>
     </h1>
 </body>

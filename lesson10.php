958d52d57cc475d0d4fcb995959da6dc2c17464d
Nathan <nmattick@gmail.com>
2020-01-15 06:54:42 +0000

    Add random age function

--- lesson10.php
+++ lesson10.php
@@ -8,9 +8,12 @@ $animals = ['dog', 'cat'];
 
 for ($i = 0; $i < 10; $i++) {
     $age = rand(1, 100);
+    echo $age . " is... ";
+
     if (allowEntry($age)) {
         echo '>21';
     } else {
         echo '<=21';
     }
+    echo "<br>";
 }

7e73a7f35f33ee822d42f8e2a3ad2e78858b9e64
Nathan <nmattick@gmail.com>
2020-01-29 06:12:28 +0000

    Learning classes

--- index.php
+++ index.php
@@ -1,13 +1,44 @@
 <?php
 
+require "functions.php";
+
 $greeting = 'Hello World'; // <?php echo"...
 
+class Task
+{
+    protected $description;
+    protected $completed = false;
+
+    public function __construct($description)
+    {
+        echo "ctor";
+        $this->description = $description;
+    }
+
+    public function description()
+    {
+        return $this->description;
+    }
+
+    public function isCompleted()
+    {
+        return $this->completed;
+    }
+
+    public function complete()
+    {
+        $hits->completed = true;
+    }
+}
+
 $task = [
-    'title' => 'homework',
-    'due' => 'today',
-    'assigned' => 'me',
-    'completed' => false,
+    new Task('Exercise'), // This is the description
+    new Task('Running'),
+    new Task('Cycling'),
 ];
 
-// require 'index.view.php';
-require 'lesson10.php';
+$task[0] . complete();
+
+// dd($task);
+
+require 'index.view.php';
